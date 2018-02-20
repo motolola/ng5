72b26bf9162c3ae8e366dfb1201138b431027d6a
motolola <motolola@icloud.com>
2018-02-20 22:48:48 +0000

    Adding route and route parameter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'; //Link to home ... no need ts
+import { AboutComponent } from './about/about.component'; //Link to home ... no need ts
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'about/:id',
+    component:AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
